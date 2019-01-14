8cf3a1df1193c86653ff9d30c4b7020cbbfbe7b4
Tateishi Tadatoshi <ishio39@gmail.com>
2019-01-14 12:15:31 +0900

    add emacs

--- initial.sh
+++ initial.sh
@@ -1,4 +1,17 @@
 #! /bin/bash
 
+export LC_ALL=C
+
+yum -y install epel-release
+
 yum install -y git
 yum install -y ansible
+ansible-galaxy install geerlingguy.docker
+ansible-galaxy install andrewrothstein.emacs
+
+yum install -y emacs
+
+mkdir wks
+cd wks
+git clone https://github.com/tateishi/ansible_local.git
+
